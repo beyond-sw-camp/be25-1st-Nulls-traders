01a7a57afd3e2f58b4ed6c4bd121044b23b50646
krystallinekim <jskim11070@gmail.com>
2026-01-20 14:31:05 +0900

    더미데이터 파일 수정

--- SQL/더미데이터/dummy_users.sql
+++ SQL/더미데이터/dummy_users.sql
@@ -5,3 +5,12 @@ INSERT INTO users VALUES
 (4, '방지혁', '01066667777', 'bangji@traders.com', 6, '정상', NOW()),
 (5, '이슬이', '01088889999', 'leeseul@traders.com', 8, '정상', NOW()),
 (6, '현재진', '01010101010', 'hyunjae@traders.com', 5, '정상', NOW());
+
+-- login 테이블 샘플데이터
+INSERT INTO login VALUES
+('minjeong1', 1, 'hashed_pw_1', 'hashed_pin_1', NOW()),
+('kimye2', 2, 'hashed_pw_2', 'hashed_pin_2', NOW()),
+('kimjeong3', 3, 'hashed_pw_3', 'hashed_pin_3', NOW()),
+('bangji4', 4, 'hashed_pw_4', 'hashed_pin_4', NOW()),
+('leeseul5', 5, 'hashed_pw_5', 'hashed_pin_5', NOW()),
+('hyunjae6', 6, 'hashed_pw_6', 'hashed_pin_6', NOW());
